6d8167677c4dbc394fbe8c9e9dae72951847ec7b
joneshf <joneshf@imail.losrios.edu>
2012-05-01 13:27:51 -0700

    Java doesn't like xmonad, so added key to toggle wm name.  Also changed keys mapping algo.

--- xmonad.hs
+++ xmonad.hs
@@ -2,12 +2,13 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName(setWMName)
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import Graphics.X11.ExtraTypes.XF86
-import Data.Map(fromList)
-import Data.Monoid(mappend)
+
+import qualified Data.Map as M
 
 main = do
     xmproc <- spawnPipe "xmobar"
@@ -33,9 +34,13 @@ myWorkspaces = [ "1: IRC"
                , "4: Code"
                ] ++ map show [5..9]
 
-myKeys = keys defaultConfig `mappend`
-            \c -> fromList
-            [ ((0, xF86XK_AudioRaiseVolume),    spawn "amixer -c 0 set Master 2dB+")
-            , ((0, xF86XK_AudioLowerVolume),    spawn "amixer -c 0 set Master 2dB-")
-            , ((0, xF86XK_AudioMute),           spawn "amixer sset Master toggle")
-            ]
+myKeys k = M.union (M.fromList (moreKeys k)) (keys defaultConfig k)
+
+moreKeys conf@(XConfig {XMonad.modMask = modm}) =
+    [ -- These two lines allow java to work with non-reparenting.
+      ((modm, xK_z),                    setWMName   "LG3D")
+    , ((modm, xK_Z),                    setWMName   "XMonad")
+    , ((0, xF86XK_AudioRaiseVolume),    spawn       "amixer -c 0 set Master 2dB+")
+    , ((0, xF86XK_AudioLowerVolume),    spawn       "amixer -c 0 set Master 2dB-")
+    , ((0, xF86XK_AudioMute),           spawn       "amixer sset Master toggle")
+    ]
