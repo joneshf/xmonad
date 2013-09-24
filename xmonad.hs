ce00189f5f8a56271d28b0f467666f7b07ebc88a
joneshf <jones3.hardy@gmail.com>
2013-09-24 10:31:08 -0700

    More inline with work stuff.

--- xmonad.hs
+++ xmonad.hs
@@ -1,46 +1,62 @@
 import XMonad
+import XMonad.Actions.CycleWS
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.SetWMName(setWMName)
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-import Graphics.X11.ExtraTypes.XF86
 
 import qualified Data.Map as M
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { terminal          = "urxvt"
+        { terminal          = "gnome-terminal"
         , modMask           = mod4Mask
         , handleEventHook   = fullscreenEventHook
-        , borderWidth       = 3
+        , borderWidth       = 2
         , workspaces        = myWorkspaces
         , keys              = myKeys
         , manageHook        = manageDocks <+> manageHook defaultConfig
         , layoutHook        = avoidStruts  $  layoutHook defaultConfig
         , logHook           = dynamicLogWithPP xmobarPP
             { ppOutput          = hPutStrLn xmproc
-            , ppTitle           = xmobarColor "green" "" . shorten 50
+            , ppTitle           = xmobarColor "green" ""
             , ppHiddenNoWindows = id
             }
         }
 
-myWorkspaces = [ "1: IRC"
-               , "2: Web"
-               , "3: Terminal"
-               , "4: Code"
-               ] ++ map show [5..9]
+myWorkspaces = [ "1: Chat"
+               , "2: Term"
+               , "3: Code"
+               , "4: Firefox"
+               , "5: Chromium"
+               ] ++ map show [6..9 :: Int]
 
 myKeys k = M.union (M.fromList (moreKeys k)) (keys defaultConfig k)
 
 moreKeys conf@(XConfig {XMonad.modMask = modm}) =
     [ -- These two lines allow java to work with non-reparenting.
-      ((modm, xK_z),                    setWMName   "LG3D")
-    , ((modm, xK_Z),                    setWMName   "XMonad")
-    , ((0, xF86XK_AudioRaiseVolume),    spawn       "amixer -c 0 set Master 2dB+")
-    , ((0, xF86XK_AudioLowerVolume),    spawn       "amixer -c 0 set Master 2dB-")
-    , ((0, xF86XK_AudioMute),           spawn       "amixer sset Master toggle")
+      ((modm, xK_z),                        setWMName   "LG3D")
+    , ((modm, xK_Z),                        setWMName   "XMonad")
+    , ((modm, xK_Left),                     prevWS)
+    , ((modm, xK_Right),                    nextWS)
+    , ((modm .|. shiftMask, xK_Left),       shiftToPrev >> prevWS)
+    , ((modm .|. shiftMask, xK_Right),      shiftToNext >> nextWS)
+    , ((mod1Mask .|. controlMask, xK_l),    spawn       "xscreensaver-command -lock")
+    , ((modm, xK_KP_Add),                   spawn       "amixer -c 1 set Master 2dB+")
+    , ((modm, xK_KP_Subtract),              spawn       "amixer -c 1 set Master 2dB-")
+    , ((modm, xK_KP_0),                     spawn       "amixer -c 1 set Master 0%")
+    , ((modm, xK_KP_1),                     spawn       "amixer -c 1 set Master 10%")
+    , ((modm, xK_KP_2),                     spawn       "amixer -c 1 set Master 20%")
+    , ((modm, xK_KP_3),                     spawn       "amixer -c 1 set Master 30%")
+    , ((modm, xK_KP_4),                     spawn       "amixer -c 1 set Master 40%")
+    , ((modm, xK_KP_5),                     spawn       "amixer -c 1 set Master 50%")
+    , ((modm, xK_KP_6),                     spawn       "amixer -c 1 set Master 60%")
+    , ((modm, xK_KP_7),                     spawn       "amixer -c 1 set Master 70%")
+    , ((modm, xK_KP_8),                     spawn       "amixer -c 1 set Master 80%")
+    , ((modm, xK_KP_8),                     spawn       "amixer -c 1 set Master 80%")
+    , ((modm, xK_KP_9),                     spawn       "amixer -c 1 set Master 90%")
+    , ((modm, xK_KP_Enter),                 spawn       "amixer sset Master toggle")
     ]
