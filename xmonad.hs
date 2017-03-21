752e9ea5793c96ad2cd005ff640c167366314c2c
joneshf <jones3.hardy@gmail.com>
2017-03-20 19:50:36 -0700

    Latest changes

--- xmonad.hs
+++ xmonad.hs
@@ -1,14 +1,23 @@
-import XMonad
+import XMonad hiding (focus)
 import XMonad.Actions.CycleWS
+import XMonad.Actions.MouseResize
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
+import XMonad.Layout
+import XMonad.Layout.Decoration
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.NoBorders (ConfigurableBorder, SetsAmbiguous(..), lessBorders, smartBorders)
+import XMonad.Layout.SimpleFloat
+import XMonad.Layout.Spacing
+import XMonad.Layout.WindowArranger
+import XMonad.StackSet (Stack(..))
 import XMonad.Util.Run
 import XMonad.Util.NamedScratchpad
 
+import Data.List (delete)
 import qualified Data.Map as M
 import Data.Monoid
 import Graphics.X11.ExtraTypes.XF86
@@ -20,11 +29,12 @@ main = do
         { terminal          = "xfce4-terminal"
         , modMask           = mod4Mask
         , handleEventHook   = fullscreenEventHook
-        , borderWidth       = 2
+        , borderWidth       = 1
+        , focusedBorderColor = "gray"
         , workspaces        = myWorkspaces
         , keys              = myKeys
         , manageHook        = namedScratchpadManageHook scratchpads <+> myManageHook <+> manageDocks <+> manageHook defaultConfig
-        , layoutHook        = smartBorders $ avoidStruts $ layoutHook defaultConfig
+        , layoutHook        = smartBorders $ avoidStruts myLayoutHook
         , logHook           = dynamicLogWithPP xmobarPP
             { ppOutput          = hPutStrLn xmproc
             , ppTitle           = xmobarColor "green" ""
@@ -46,6 +56,7 @@ myManageHook = composeAll
     , isDialog                    --> doFloat
     , className =? "Pidgin"       --> doFloat
     , className =? "Lxappearance" --> doFloat
+    , className =? "Wish" 	  --> doFloat
     , "Xchat"    `shiftClassTo` "1: Chat"
     , "Pidgin"   `shiftClassTo` "1: Chat"
     --, "Chromium" `shiftClassTo` "4: Web"
@@ -76,8 +87,10 @@ moreKeys (XConfig {XMonad.modMask = modm}) =
     -- Lock the screen.
     , ((mod1Mask .|. controlMask, xK_l),            spawn "xscreensaver-command -lock")
     -- Brightness.
-    , ((modm,                     xF86XK_MonBrightnessUp),   spawn "xbacklight -inc 10")
-    , ((modm,                     xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
+    , ((modm,                     xF86XK_MonBrightnessUp),   spawn "xbacklight -inc 1")
+    , ((modm,                     xF86XK_MonBrightnessDown), spawn "xbacklight -dec 1")
+    , ((modm .|. shiftMask,       xF86XK_MonBrightnessUp),   spawn "xbacklight -inc 10")
+    , ((modm .|. shiftMask,       xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
     -- Volume controls.
     , ((0,                        xF86XK_AudioLowerVolume), spawn "amixer -c 1 set Master 2dB-")
     , ((0,                        xF86XK_AudioRaiseVolume), spawn "amixer -c 1 set Master 2dB+")
@@ -85,4 +98,28 @@ moreKeys (XConfig {XMonad.modMask = modm}) =
     , ((modm .|. controlMask .|. shiftMask, xK_t), namedScratchpadAction scratchpads "htop")
     -- dmenu.
     , ((modm,                     xK_p),            spawn "dmenu_run -fn \"xft:Droid Sans Mono\"")
+    -- Screenshot.
+    , ((mod1Mask .|. shiftMask, xK_3), spawn "import -window root ~/screenshots/screenshot-$(date +%s).png")
+    , ((mod1Mask .|. shiftMask, xK_4), spawn "import ~/screenshots/screenshot-$(date +%s).png")
     ]
+
+myLayoutHook ::
+    ModifiedLayout
+        (ConfigurableBorder MyAmbiguity)
+        (ModifiedLayout
+            SmartSpacingWithEdge
+            (Choose
+                (Choose Tall (Choose (Mirror Tall) Full))
+                (ModifiedLayout
+                    (Decoration SimpleDecoration DefaultShrinker)
+                    (ModifiedLayout
+                        MouseResize
+                        (ModifiedLayout WindowArranger SimpleFloat)))))
+        Window
+myLayoutHook = lessBorders MyAmbiguity $ smartSpacingWithEdge 4 $ layoutHook defaultConfig ||| simpleFloat
+
+data MyAmbiguity = MyAmbiguity deriving (Read, Show)
+
+instance SetsAmbiguous MyAmbiguity where
+    hiddens _ _ Nothing xs = fst <$> init xs
+    hiddens _ _ (Just (Stack {focus = x})) xs = delete x (fst <$> xs)
